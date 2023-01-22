1b9387735d1bbb60ddd4c1a7e58691a25240beb9
abriel0402 <abchprod@gmail.com>
2023-01-21 19:23:03 -0500

    views update

--- mysite/websiteapp/urls.py
+++ mysite/websiteapp/urls.py
@@ -3,4 +3,6 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("projects/", views.projects, name="projects"),
+    path("links/", views.links, name="links"),
 ]
--- mysite/websiteapp/views.py
+++ mysite/websiteapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "websiteapp/index.html")
+
+def projects(request):
+    return render(request, "websiteapp/projects.html")
+
+def links(request):
+    return render(request, "websiteapp/links.html")
